b505dddc673ad669642e525f4dd51e59ec237901
Fernando Lomas García <fernando.lomas@inteco.local>
2020-01-14 10:21:40 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import * as moment from 'moment';
+import * as introJs from 'intro.js/intro.js';
 import { MessageService } from './core/utils/message.service';
 
 @Component({
@@ -9,6 +10,7 @@ import { MessageService } from './core/utils/message.service';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+    introJS = introJs();
     title = 'miapp';
     myDate = null;
 
@@ -25,10 +27,44 @@ export class AppComponent implements OnInit {
         this.msgService.showError('clave', 'Texto a mostrar');
 
         this.myDate = new Date();
+
+        this.introJS.setOptions({
+            steps: [
+                {
+                    intro: 'Hello world! patatín patatán. <p>este es un párrafo</p> Y bla bla bla bla'
+                },
+                {
+
+                    element: document.querySelector('#step3'),
+                    intro: 'This is a tooltip.'
+                },
+                {
+                    element: document.querySelectorAll('#step2')[0],
+                    intro: 'Ok, wasn\'t that fun?',
+                    position: 'right'
+                },
+                {
+                    element: '#step3',
+                    intro: 'More features, more fun.',
+                    position: 'left'
+                },
+                {
+                    element: '.step4',
+                    intro: 'Another step.',
+                    position: 'bottom'
+                },
+                {
+                    element: '#step5',
+                    intro: 'Get it, use it.'
+                }
+            ]
+        });
     }
 
     ngOnInit(): void {
         this.msgService.showSuccess('clave', 'Texto a mostrar');
         this.msgService.showError('clave', 'Texto a mostrar');
+
+        this.introJS.start();
     }
 }
